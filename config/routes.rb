e9ac151a97ac1e120ae5d028339f05ba52920daf
wangminyi <305463144@qq.com>
2017-03-31 00:51:51 +0800

    U

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-  
+
   scope module: :home do
     get :sign_in
   end
-
-  mount ActionCable.server => '/websocket'
 end
