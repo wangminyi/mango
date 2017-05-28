f35e8830c38f37f07039f2dafeda97ca49b58486
wangminyi <305463144@qq.com>
2017-05-28 12:27:40 +0800

    fix bug

--- app/models/lib/wx.rb
+++ app/models/lib/wx.rb
@@ -1,13 +1,13 @@
 class Wx
   class << self
     def access_token
-      return nil if !Rails.production_group?
+      return nil if !Rails.env.production?
 
       token_redis = Redis::Value.new("wx_access_token")
       if token_redis.value.nil?
         id = ENV["wx_id"]
         secret = ENV["wx_secret"]
-        res = JSON.parse(RestClient.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{appid}&secret=#{appsecret}"))
+        res = JSON.parse(RestClient.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{id}&secret=#{secret}"))
         token_redis.value = res["access_token"]
         token_redis.expire res["expires_in"].seconds
       end
