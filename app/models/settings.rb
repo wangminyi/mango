1ea41eba69f17dfa05b283533d25a3d06671e7c1
wangminyi <305463144@qq.com>
2017-06-25 17:19:03 +0800

    更新小区名

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -3,10 +3,14 @@ class Settings
   FREE_DISTRIBUTION = 1000
   PREFERENTIAL_PRICE = 300
   GARDENS = %w(
+              周浦兰廷九龙仓
               周浦印象春城
-              周浦逸亭佳苑
-              周浦兰亭-九龙仓
-              周浦惠康公寓
+              周浦明天华城
+              周浦繁荣华庭
+              周浦中金海棠湾
+              周浦御沁园
+              周浦绿地梧桐苑
+              周浦绿地景汇佳苑
             )
   class << self
     def as_json
